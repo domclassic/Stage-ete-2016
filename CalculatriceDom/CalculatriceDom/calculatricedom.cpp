3ea807b024265b18b168f3a61fa316de7e236fc3
domclassic <dominic.pichette@gmail.com>
2016-05-30 11:46:54 -0400

    Update calculatricedom.cpp

--- CalculatriceDom/CalculatriceDom/calculatricedom.cpp
+++ CalculatriceDom/CalculatriceDom/calculatricedom.cpp
@@ -5,97 +5,85 @@
 CalculatriceDom::CalculatriceDom(QWidget *parent)
 	: QMainWindow(parent)
 {
+	//bLayout = new QVBoxLayout;
+
+	//output
+	label = new QLabel("0", this);
+
 	//grid layout
 	grille = new QGridLayout;
 
-
-
-	//output
-	/*label = new QLabel("0", this);
-	grille->addWidget(label, 0, 0, 3, 1);*/
-	//label->setGeometry(QRect(QPoint(10, 10), QSize(200, 100)));
-	
-
 	//bouttons digit
-	un_button = new QPushButton("1", this);
-	//un_button->setGeometry(QRect(QPoint(10, 200), QSize(50, 50)));
-	grille->addWidget(un_button, 0, 0);
-	connect(un_button, SIGNAL(released()), this, SLOT(un()));
-
-
-	deux_button = new QPushButton("2", this);
-	//deux_button->setGeometry(QRect(QPoint(60, 200), QSize(50, 50)));
-	grille->addWidget(un_button, 0, 1);
-	connect(deux_button, SIGNAL(released()), this, SLOT(deux()));
-
-	/*trois_button = new QPushButton("3", this);
-	//trois_button->setGeometry(QRect(QPoint(110, 200), QSize(50, 50)));
-	grille->addWidget(un_button, 2, 1);
-	connect(trois_button, SIGNAL(released()), this, SLOT(trois()));
-
-	quatre_button = new QPushButton("4", this);
-	//quatre_button->setGeometry(QRect(QPoint(10, 150), QSize(50, 50)));
-	grille->addWidget(un_button, 0, 2);
-	connect(quatre_button, SIGNAL(released()), this, SLOT(quatre()));
-
-	cinq_button = new QPushButton("5", this);
-	//cinq_button->setGeometry(QRect(QPoint(60, 150), QSize(50, 50)));
-	grille->addWidget(un_button, 1, 2);
-	connect(cinq_button, SIGNAL(released()), this, SLOT(cinq()));
-
-	six_button = new QPushButton("6", this);
-	//six_button->setGeometry(QRect(QPoint(110, 150), QSize(50, 50)));
-	grille->addWidget(un_button, 2, 2);
-	connect(six_button, SIGNAL(released()), this, SLOT(six()));
-
 	sept_button = new QPushButton("7", this);
-	//sept_button->setGeometry(QRect(QPoint(10, 100), QSize(50, 50)));
-	grille->addWidget(sept_button, 0, 1);
+	grille->addWidget(sept_button, 0, 0);
 	connect(sept_button, SIGNAL(released()), this, SLOT(sept()));
 
 	huit_button = new QPushButton("8", this);
-	//huit_button->setGeometry(QRect(QPoint(60, 100), QSize(50, 50)));
-	grille->addWidget(un_button, 1, 1);
+	grille->addWidget(un_button, 0, 1);
 	connect(huit_button, SIGNAL(released()), this, SLOT(huit()));
 
 	neuf_button = new QPushButton("9", this);
-	//neuf_button->setGeometry(QRect(QPoint(110, 100), QSize(50, 50)));
-	grille->addWidget(un_button, 2, 1);
+	grille->addWidget(un_button, 0, 2);
 	connect(neuf_button, SIGNAL(released()), this, SLOT(neuf()));
 
+	quatre_button = new QPushButton("4", this);
+	grille->addWidget(un_button, 1, 0);
+	connect(quatre_button, SIGNAL(released()), this, SLOT(quatre()));
+
+	cinq_button = new QPushButton("5", this);
+	grille->addWidget(un_button, 1, 1);
+	connect(cinq_button, SIGNAL(released()), this, SLOT(cinq()));
+
+	six_button = new QPushButton("6", this);
+	grille->addWidget(un_button, 1, 2);
+	connect(six_button, SIGNAL(released()), this, SLOT(six()));
+
+	un_button = new QPushButton("1", this);
+	grille->addWidget(un_button, 2, 0);
+	connect(un_button, SIGNAL(released()), this, SLOT(un()));
+
+	deux_button = new QPushButton("2", this);
+	grille->addWidget(un_button, 2, 1);
+	connect(deux_button, SIGNAL(released()), this, SLOT(deux()));
+
+	trois_button = new QPushButton("3", this);
+	grille->addWidget(un_button, 2, 2);
+	connect(trois_button, SIGNAL(released()), this, SLOT(trois()));
+
 	zero_button = new QPushButton("0", this);
-	//zero_button->setGeometry(QRect(QPoint(10, 250), QSize(50, 50)));
-	grille->addWidget(un_button, 1, 4);
-	connect(zero_button, SIGNAL(released()), this, SLOT(zero()));*/
+	grille->addWidget(un_button, 4, 1);
+	connect(zero_button, SIGNAL(released()), this, SLOT(zero()));
 
 	//bouttons op.
-	/*add_button = new QPushButton("+", this);
-	add_button->setGeometry(QRect(QPoint(160, 100), QSize(50, 50)));
+	add_button = new QPushButton("+", this);
+	grille->addWidget(un_button, 0, 3);
 	connect(add_button, SIGNAL(released()), this, SLOT(add()));
 
 	sous_button = new QPushButton("-", this);
-	sous_button->setGeometry(QRect(QPoint(160, 150), QSize(50, 50)));
+	grille->addWidget(un_button, 1, 3);
 	connect(sous_button, SIGNAL(released()), this, SLOT(sous()));
 
 	mult_button = new QPushButton("*", this);
-	mult_button->setGeometry(QRect(QPoint(160, 200), QSize(50, 50)));
+	grille->addWidget(un_button, 2, 3);
 	connect(mult_button, SIGNAL(released()), this, SLOT(mult()));
 
 	divi_button = new QPushButton("/", this);
-	divi_button->setGeometry(QRect(QPoint(160, 250), QSize(50, 50)));
+	grille->addWidget(un_button, 3, 3);
 	connect(divi_button, SIGNAL(released()), this, SLOT(divi()));
 
 	egal_button = new QPushButton("=", this);
-	egal_button->setGeometry(QRect(QPoint(110, 250), QSize(50, 50)));
+	grille->addWidget(un_button, 4, 3);
 	connect(egal_button, SIGNAL(released()), this, SLOT(egal()));
 
 	clear_button = new QPushButton("C", this);
-	clear_button->setGeometry(QRect(QPoint(60, 250), QSize(50, 50)));
-	connect(clear_button, SIGNAL(released()), this, SLOT(clear()));*/
+	grille->addWidget(un_button, 4, 2);
+	connect(clear_button, SIGNAL(released()), this, SLOT(clear()));
 
 
 
-	//unWidget->setLayout(grille);
+	//bLayout->addWidget(label, 0, 0);
+	//bLayout->addLayout(grille);
+	//this->setLayout(grille);
 
 
 }
@@ -108,40 +96,28 @@ void CalculatriceDom::add() {
 	num1 = value.toInt();
 	value = "";
 	label->setText(value);
-	addBool = true;
-	sousBool = false;
-	multBool = false;
-	diviBool = false;
+	op = operation::Addi;
 }
 
 void CalculatriceDom::sous() {
 	num1 = value.toInt();
 	value = "";
 	label->setText(value);
-	addBool = false;
-	sousBool = true;
-	multBool = false;
-	diviBool = false;
+	op = operation::Sous;
 }
 
 void CalculatriceDom::mult() {
 	num1 = value.toInt();
 	value = "";
 	label->setText(value);
-	addBool = false;
-	sousBool = false;
-	multBool = true;
-	diviBool = false;
+	op = operation::Mult;
 }
 
 void CalculatriceDom::divi() {
 	num1 = value.toInt();
 	value = "";
 	label->setText(value);
-	addBool = false;
-	sousBool = false;
-	multBool = false;
-	diviBool = true;
+	op = operation::Divi;
 }
 
 void CalculatriceDom::zero() {
@@ -197,19 +173,19 @@ void CalculatriceDom::neuf() {
 void CalculatriceDom::egal() {
 	num2 = value.toInt();
 	value = "";
-	if (addBool) {
+	if (op == operation::Addi) {
 		total = QString::number(num1+num2);
 		label->setText(total);
 	}
-	else if (sousBool) {
+	else if (op == operation::Sous) {
 		total = QString::number(num1 - num2);
 		label->setText(total);
 	}
-	else if (multBool) {
+	else if (op == operation::Mult) {
 		total = QString::number(num1 * num2);
 		label->setText(total);
 	}
-	else if (diviBool) {
+	else if (op == operation::Divi) {
 		total = QString::number(num1 / num2);
 		label->setText(total);
 	}
